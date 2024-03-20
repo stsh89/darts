e5389b05d6d9fb6d2995d15dc7565d58cd437d49
Stanislav Shandyga <stanislavshandyga@gmail.com>
2024-03-20 09:01:56 +0200

    Minor tests update

--- dataspine/tests/integration/helpers.rs
+++ dataspine/tests/integration/helpers.rs
@@ -1,17 +1,16 @@
-use sqlx::{PgPool, Row};
+use sqlx::PgPool;
 use uuid::Uuid;
 
 pub async fn get_game_id(pool: &PgPool) -> anyhow::Result<Uuid> {
-    let id = sqlx::query("SELECT id FROM playground.games LIMIT 1")
+    let id = sqlx::query_scalar("SELECT id FROM playground.games LIMIT 1")
         .fetch_one(pool)
-        .await?
-        .get("id");
+        .await?;
 
     Ok(id)
 }
 
 pub async fn get_score_id(pool: &PgPool) -> anyhow::Result<Uuid> {
-    let id = sqlx::query(
+    let id = sqlx::query_scalar(
         r#"
 SELECT id
 FROM playground.scores
@@ -19,26 +18,23 @@ ORDER BY round_number DESC, player_number DESC LIMIT 1
         "#,
     )
     .fetch_one(pool)
-    .await?
-    .get("id");
+    .await?;
 
     Ok(id)
 }
 
 pub async fn count_games(pool: &PgPool) -> anyhow::Result<i64> {
-    let count = sqlx::query("SELECT COUNT(*) count FROM playground.games")
+    let count = sqlx::query_scalar("SELECT COUNT(*) count FROM playground.games")
         .fetch_one(pool)
-        .await?
-        .get("count");
+        .await?;
 
     Ok(count)
 }
 
 pub async fn count_scores(pool: &PgPool) -> anyhow::Result<i64> {
-    let count = sqlx::query("SELECT COUNT(*) count FROM playground.scores")
+    let count = sqlx::query_scalar("SELECT COUNT(*) count FROM playground.scores")
         .fetch_one(pool)
-        .await?
-        .get("count");
+        .await?;
 
     Ok(count)
 }
