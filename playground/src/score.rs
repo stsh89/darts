270743c371cb559eb68a7315af3eb380acdeb995
Stanislav Shandyga <stanislavshandyga@gmail.com>
2024-03-29 14:33:52 +0200

    Minor code update

--- playground/src/score.rs
+++ playground/src/score.rs
@@ -8,45 +8,35 @@ const POINTS_LIMIT: u16 = 180;
 pub struct Score(Points);
 
 impl Score {
-    pub fn new(points: u16) -> Result<Self, Error> {
-        ScoreBuilder::new().points(Points::new(points)).build()
-    }
-
-    pub fn points(&self) -> Points {
-        self.0
-    }
-}
-
-struct ScoreBuilder {
-    points: Result<Points, Error>,
-}
-
-impl ScoreBuilder {
-    fn new() -> Self {
-        Self {
-            points: Err(Error::InvalidArgument("Missing score points".to_string())),
-        }
-    }
-
-    fn points(mut self, points: Points) -> Self {
-        self.points = if points > Points::new(POINTS_LIMIT) {
+    fn assign_points(&mut self, points: Points) -> Result<(), Error> {
+        if points > Points::new(POINTS_LIMIT) {
             let description = format!(
                 "The maximum number of points allowed is {}. Given: {}",
                 POINTS_LIMIT, points
             );
 
-            Error::invalid_argument(description).into()
-        } else {
-            Ok(points)
+            return Error::invalid_argument(description).into();
         };
 
-        self
+        self.0 = points;
+
+        Ok(())
     }
 
-    fn build(self) -> Result<Score, Error> {
-        let points = self.points?;
+    fn init() -> Self {
+        Self(Points::zero())
+    }
 
-        Ok(Score(points))
+    pub fn new(points: u16) -> Result<Self, Error> {
+        let mut score = Score::init();
+
+        score.assign_points(Points::new(points))?;
+
+        Ok(score)
+    }
+
+    pub fn points(&self) -> Points {
+        self.0
     }
 }
 
